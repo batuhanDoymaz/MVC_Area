6f9e4ff5de55435f48051e790a326757651fe4b8
baduwan <batuhan.doymaz@gmail.com>
2023-01-26 16:36:28 +0300

    Add project files.

---
+++ MVC_Area/Areas/Dashboard/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Area.Areas.Dashboard.Controllers
+{
+    [Area("Dashboard")]
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
